511b9b6b55475457652a676705c03128df0fefc9
Hazieon <handrew3721@gmail.com>
2021-03-30 01:44:40 +0100

    split queries by comma, search multiple queries and store results in state

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import Search from "./components/search";
 require("dotenv").config();
 
+//could pass each query down here to search component children
 function App() {
   return (
     <div className="App">
+      <h1>MultiTube</h1>
       <Search />
     </div>
   );
--- src/components/search/index.js
+++ src/components/search/index.js
@@ -4,41 +4,44 @@ const KEY = process.env.REACT_APP_API_KEY;
 //https://www.googleapis.com/youtube/v3/search?part=snippet&key=${KEY}&type=video&q=blue%20moon
 function Search() {
   const [query, setQuery] = useState("");
+  const [queryArray, setQueryArray] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
-
-  //   useEffect(() => {
-  //     runSearch();
-  //   }, [query]);
 
   function queryHandler(text) {
     setQuery(text);
-    console.log(query);
   }
   async function runSearch() {
+    //split the list of queries at commas
+    let newQuery = query.split(", ");
+
+    // newQuery - run search on each of them - forEach
+    newQuery.forEach((element) => {
+      runSearchQuery(element);
+    });
+  }
+
+  async function runSearchQuery(query) {
     let response = await fetch(
       `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&key=${KEY}&type=video&q=${query}`
     );
     let data = await response.json();
-    setSearchResults(data.items);
-    console.log(searchResults);
-    console.log(searchResults.length);
+    setQueryArray([...queryArray, data]);
+    setSearchResults(queryArray);
   }
 
   return (
     <>
-      <input
+      <h3>Search with a comma separated list here:</h3>
+      <textarea
         type="text"
         placeholder="search..."
         onChange={(e) => queryHandler(e.target.value)}
-      ></input>
+      ></textarea>
       <button onClick={runSearch}>search</button>
 
       {searchResults.length > 0
         ? searchResults.map((item) => {
-            console.log(item.snippet.title);
-            return (
-              <p key={item.snippet.thumbnails.title}>{item.snippet.title}</p>
-            );
+            return <p key={item.snippet.title}>{item.snippet.title}</p>;
           })
         : ""}
     </>
@@ -46,3 +49,8 @@ function Search() {
 }
 
 export default Search;
+
+// let response = await fetch(
+//   `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&key=${KEY}&type=video&q=${query}`
+// );
+// let data = await response.json();
